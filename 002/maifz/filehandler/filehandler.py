8dc5ddbbcdfa7e8d1877fb81a837cc9bf36c5c63
maifz <ae.work.machine@gmail.com>
2019-10-04 20:43:36 +0200

    fix get, when file is not existing

--- 002/maifz/filehandler/filehandler.py
+++ 002/maifz/filehandler/filehandler.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class FileHandler():
 
     def appendToFile(self, file_path, data):
@@ -5,5 +8,9 @@ class FileHandler():
             file.write(data.decode('utf-8') + '\n\r')
 
     def readFile(self, file_path):
-        with open(file_path, 'rb') as file:
-            return file.read()
+        try:
+            with open(file_path, 'rb') as file:
+                return file.read()
+        except FileNotFoundError:
+            print("Your html file doesn't exist")
+            sys.exit(1)
