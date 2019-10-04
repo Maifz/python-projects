9ce5afda73173126e64cba8c7b37d5dc6f3039a8
Alexandra Eschrich <ae@plista.com>
2019-10-04 16:39:30 +0200

    WIP - webserver, file handler class, args helper class

---
+++ 002/maifz/filehandler/file_handler.py
@@ -0,0 +1,10 @@
+class FileHandler():
+
+	def appendToFile(self, file_path, data):
+		with open(file_path, 'a') as file:
+			file.write(data + '\n\r')
+
+	def readFile(self):
+		with open('index.html', 'rb') as file:
+			file.read()
+		print("read")
