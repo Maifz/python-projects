9e760b03f42fedf6de8aedae93bbeed0936fff55
Alexandra Eschrich <ae@plista.com>
2019-10-04 16:50:14 +0200

    reformat code

--- 002/maifz/argshelper/args_helper.py
+++ 002/maifz/argshelper/args_helper.py
@@ -1,24 +1,24 @@
 class ArgsHelper:
-	__instance = None
+    __instance = None
 
-	content_type = "text/plain"
+    content_type = "text/plain"
 
-	@staticmethod
-	def getInstance():
-		""" Static access method. """
-		if ArgsHelper.__instance == None:
-			ArgsHelper()
-		return ArgsHelper.__instance
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if ArgsHelper.__instance == None:
+            ArgsHelper()
+        return ArgsHelper.__instance
 
-	def __init__(self):
-		""" Virtually private constructor. """
-		if ArgsHelper.__instance != None:
-			raise Exception("This class is a singleton!")
-		else:
-			ArgsHelper.__instance = self
+    def __init__(self):
+        """ Virtually private constructor. """
+        if ArgsHelper.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            ArgsHelper.__instance = self
 
-	def setContentType(self, content_type):
-		self.content_type = content_type
+    def setContentType(self, content_type):
+        self.content_type = content_type
 
-	def getContentType(self):
-		return self.content_type
+    def getContentType(self):
+        return self.content_type
