85e5e3691e106193698faeecdf5df2fa1c477cf2
maifz <ae.work.machine@gmail.com>
2019-10-04 19:27:23 +0200

    added end_headers function to get response

--- 002/maifz/filehandler/filehandler.py
+++ 002/maifz/filehandler/filehandler.py
@@ -2,8 +2,8 @@ class FileHandler():
 
     def appendToFile(self, file_path, data):
         with open(file_path, 'a') as file:
-            file.write(str(data) + '\n\r')
+            file.write(data.decode('utf-8') + '\n\r')
 
     def readFile(self, file_path):
-        with open('index.html', 'rb') as file:
+        with open(file_path, 'rb') as file:
             return file.read()
